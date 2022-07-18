82a38683bdbeedb3f89f173a4da23b56cd220db4
maxembo <91334358+maxembo@users.noreply.github.com>
2022-07-18 15:12:20 +0300

    Add project files.

---
+++ PracticalWork2.7/Example1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Example
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fullName = "Popov Maxim Romanovich", email = "maxim.popov1234@mail.ru";
+            double progrScores = 65.6, mathScores = 78.3, physicsScores = 59.1;
+            int age = 20;
+
+            Console.WriteLine($"ФИО: {fullName}\n" +
+                              $"Возраст: {age}\n" +
+                              $"Email: {email}\n" +
+                              $"Баллы по программированию: {progrScores}\n" +
+                              $"Баллы по математике: {mathScores}\n" +
+                              $"Баллы по физике: {physicsScores}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PracticalWork2.7/Example2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace Example2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  ЕГЭ  ");
+            Console.Write("Введите сумму баллов: ");
+            double sumScores = double.Parse(Console.ReadLine());
+            Console.Write("Введите количество сданных предметов: ");
+            int subjects = int.Parse(Console.ReadLine());
+            double averageScore = (double)(sumScores / subjects);
+
+            Console.WriteLine($"Средний балл:{Math.Round(averageScore,1)}");
+
+            Console.ReadKey();
+        }
+    }
+}
