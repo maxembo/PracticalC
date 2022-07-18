e2c645029b69279db532cd6989332590994fe870
maxembo <91334358+maxembo@users.noreply.github.com>
2022-07-18 15:17:19 +0300

    ready Example1,Example2(PracticalWork5.5)

---
+++ PracticalWork5.5/Example1_Method/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Example1_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            Split(Console.ReadLine());
+        }
+        /// <summary>
+        /// Метод,выполняющий разделение строки на слова
+        /// </summary>
+        /// <param name="senctence"></param>
+        static void Split(string senctence)
+        {
+            string[] words = senctence.Split(' ', StringSplitOptions.RemoveEmptyEntries);//Параметр StringSplitOptions.RemoveEmptyEntries удаляет все пустые подстроки
+
+            Console.WriteLine("Результат:");
+            foreach (string result in words)
+            {
+                Console.WriteLine($"{result}");
+            }
+        }
+    }
+}
---
+++ PracticalWork5.5/Example2_Method/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Example2_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите предложение: ");
+            Split(Console.ReadLine());
+        }
+        /// <summary>
+        /// Метод,выполняющий разделение строки на слова и перестановку слов в предложении
+        /// </summary>
+        /// <param name="senctence"></param>
+        static void Split(string senctence)
+        {
+            string[] words = senctence.Split(' ', StringSplitOptions.RemoveEmptyEntries);//Параметр StringSplitOptions.RemoveEmptyEntries удаляет все пустые подстроки
+
+            Console.WriteLine("Результат:");
+            ReversWords(words);
+        }
+        /// <summary>
+        /// Метод, выполняющий перестановку слов в предложении
+        /// </summary>
+        /// <param name="words"></param>
+        static void ReversWords(string[] words)
+        {
+            for (int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine($"{words[i]}");
+            }
+        }
+    }
+}
